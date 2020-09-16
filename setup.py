0e6b5f8db88bdb630b48fdd084f5044f504f1e2d
Rohit Khattar <rohitkh@byu.edu>
2020-09-16 20:02:53 +0000

    Warehouse_Commit

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_namespace_packages
-from tethys_apps.app_installation import find_resource_files
+from setup_helper import find_resource_files
 
 ### Apps Definition ###
 app_package = 'embalses'
@@ -13,6 +13,7 @@ resource_files += find_resource_files('tethysapp/' + app_package + '/workspaces'
 ### Python Dependencies ###
 dependencies = []
 
+resource_files += find_resource_files('tethysapp/' + app_package + '/scripts', 'tethysapp/' + app_package)
 setup(
     name=release_package,
     version='1.0.0',
